d882b41e901f4f5a52bd7306f89e305e8dcad371
bjimison <brady.t.jimison@gmail.com>
2018-08-24 13:54:29 -0700

    project setup

---
+++ server/server.js
@@ -0,0 +1,16 @@
+// require Express, create an Express app
+const cors = require("cors");
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+
+app.use(cors());
+
+// add the body-parser middleware to the server
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// listen on the port that Heroku prescribes (process.env.PORT) OR port 3000
+app.listen(process.env.PORT || 4000, () => {
+  console.log("Express server is up and running on http://localhost:4000/");
+});
