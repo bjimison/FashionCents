215b9cef395c44ccdf3c3c2302edae9f2f4df287
bjimison <brady.t.jimison@gmail.com>
2018-09-04 11:28:17 -0700

    styling sidebar

--- fashioncents/src/components/Sidebar.js
+++ fashioncents/src/components/Sidebar.js
@@ -3,9 +3,12 @@ import React, {Component} from 'react';
 class Sidebar extends Component {
     render(){
         return(<div id="sidebar-container">
-            <ul id="sidebar-list">
+            <ul id="sidebar-list1">
             <li>Hats</li>
             <li>Jackets</li>
+            </ul>
+            <i class="fas fa-chevron-circle-left"></i>
+            <ul id="sidebar-list2">
             <li>Shirts</li>
             <li>Pants</li>
             </ul>
