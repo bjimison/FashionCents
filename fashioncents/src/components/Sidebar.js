d12f8701642f8625c6d4cdac10ec67087d265161
bjimison <brady.t.jimison@gmail.com>
2018-09-24 15:21:17 -0700

    fixed sidebar error and added media queries to search input

--- fashioncents/src/components/Sidebar.js
+++ fashioncents/src/components/Sidebar.js
@@ -1,56 +1,98 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
 
-
 class Sidebar extends Component {
-
-     toggle_visibility = () => {
-       let e = document.getElementById('sidebar-container');
-        let right_arrow = document.getElementById('right-arrow');
-        let left_arrow = document.getElementById('left-arrow');
-        let posts = document.querySelector(".posts");
-        let search = document.querySelector(".search");
-       if(e.style.display === 'inline-block'){
-          e.style.display = 'none';
-        right_arrow.style.display = 'inline-block';
-        left_arrow.style.display = 'none';
-        search.style.marginLeft = '0';
-        posts.style.width = '94%';
-        posts.style.marginRight = '3%';
-        posts.style.marginLeft = '3%';
-      } else {
-          e.style.display = 'inline-block';
-          right_arrow.style.display = 'none';
-          left_arrow.style.display = 'block';
-          posts.style.width = '83%';
-          search.style.marginLeft = '10%';
-          }
+  toggle_visibility = () => {
+    let e = document.getElementById("sidebar-container");
+    let right_arrow = document.getElementById("right-arrow");
+    let left_arrow = document.getElementById("left-arrow");
+    let posts = document.querySelector(".posts");
+    let search = document.querySelector(".search");
+    if (e.style.display === "inline-block") {
+      e.style.display = "none";
+      right_arrow.style.display = "inline-block";
+      left_arrow.style.display = "none";
+      //   search.style.marginLeft = "0";
+      posts.style.width = "94%";
+      posts.style.marginRight = "3%";
+      posts.style.marginLeft = "3%";
+    } else {
+      e.style.display = "inline-block";
+      right_arrow.style.display = "none";
+      left_arrow.style.display = "block";
+      posts.style.width = "83%";
+      //   search.style.marginLeft = '10%';
     }
+  };
 
-    render(){
-        return(
-            <div>
-            <div id="sidebar-container">
-            <ul id="sidebar-list">
-                <li className="nav-item-side"><span onClick={this.props.getAllPosts} id="all" to={`/posts/${this.props.post_category}`}>
-                    All</span></li>
-                <li className="nav-item-side"><span onClick={this.props.categorySelect} id="hats" to={`/posts/${this.props.post_category}`}>
-                    Hats</span></li>
-                <li className="nav-item-side"><span onClick={this.props.categorySelect} id="jackets" to={`/posts/${this.props.post_category}`}>
-                    Jackets</span></li>
-                <li className="nav-item-side"><span onClick={this.props.categorySelect} id="shirts" to={`/posts/${this.props.post_category}`}>
-                    Shirts</span></li>
-                <li className="nav-item-side"><span onClick={this.props.categorySelect} id="pants" to={`/posts/${this.props.post_category}`}>
-                    Pants</span></li>
-            </ul>
-            </div>
-        
-            <div id="arrows">
-                <i id="right-arrow" className="fas fa-chevron-circle-right" onClick={this.toggle_visibility}></i>
-                <i id="left-arrow" className="fas fa-chevron-circle-left" onClick={this.toggle_visibility}></i>
-            </div>
+  render() {
+    return (
+      <div>
+        <div id="sidebar-container">
+          <ul id="sidebar-list">
+            <li className="nav-item-side">
+              <span
+                onClick={this.props.getAllPosts}
+                id="all"
+                to={`/posts/${this.props.post_category}`}
+              >
+                All
+              </span>
+            </li>
+            <li className="nav-item-side">
+              <span
+                onClick={this.props.categorySelect}
+                id="hats"
+                to={`/posts/${this.props.post_category}`}
+              >
+                Hats
+              </span>
+            </li>
+            <li className="nav-item-side">
+              <span
+                onClick={this.props.categorySelect}
+                id="jackets"
+                to={`/posts/${this.props.post_category}`}
+              >
+                Jackets
+              </span>
+            </li>
+            <li className="nav-item-side">
+              <span
+                onClick={this.props.categorySelect}
+                id="shirts"
+                to={`/posts/${this.props.post_category}`}
+              >
+                Shirts
+              </span>
+            </li>
+            <li className="nav-item-side">
+              <span
+                onClick={this.props.categorySelect}
+                id="pants"
+                to={`/posts/${this.props.post_category}`}
+              >
+                Pants
+              </span>
+            </li>
+          </ul>
         </div>
-        )}
+
+        <div id="arrows">
+          <i
+            id="right-arrow"
+            className="fas fa-chevron-circle-right"
+            onClick={this.toggle_visibility}
+          />
+          <i
+            id="left-arrow"
+            className="fas fa-chevron-circle-left"
+            onClick={this.toggle_visibility}
+          />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Sidebar;
