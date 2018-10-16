f11d10c6ecd4d6d2f9cd7fd746845c7a829d213a
bjimison <brady.t.jimison@gmail.com>
2018-10-16 08:43:31 -0700

    cleaning code indentation

--- fashioncents/src/models/getPostByCategory.js
+++ fashioncents/src/models/getPostByCategory.js
@@ -2,8 +2,9 @@ import axios from "axios";
 
 class getPostByCategory {
   static getCategory(post_category) {
-      console.log("from model api call - Category:", post_category)
-    let request = axios.get(`http://localhost:4000/api/posts/category/${post_category}`);
+    let request = axios.get(
+      `http://localhost:4000/api/posts/category/${post_category}`
+    );
     return request;
   }
 }
--- fashioncents/src/models/getPosts.js
+++ fashioncents/src/models/getPosts.js
@@ -3,7 +3,6 @@ import axios from "axios";
 class getPosts {
   static getAll(data) {
     let request = axios.get("http://localhost:4000/api/posts/");
-    console.log(request);
     return request;
   }
 }
